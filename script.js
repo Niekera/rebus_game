c24b7487a28ff4053883046a30fcb6f4e7c8f061
entrovertt <117159827+Niekera@users.noreply.github.com>
2022-11-08 12:39:02 +0200

    Мацевич Назарій зробив завдання, довиконуються деталі

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+var input1=document.getElementById("task1"),
+    res=document.getElementById("result"),
+    button=document.getElementById("button"),
+    counter=0
+    button.addEventListener('click',function add(){
+    if(input1.value=='Github'){
+    counter=counter+1
+    res.innerHTML='Your result'+' ' + counter
+    
+}
+
+    } )
+    
